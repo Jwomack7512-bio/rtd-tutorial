0534e82e37b674c1a99e4f8f7006412302f8b9e3
Jwomack7512-bio <78865343+Jwomack7512-bio@users.noreply.github.com>
2023-02-11 18:04:28 -0600

    updated main fxns

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
+project = 'BioModME'
 copyright = '2021, Graziella'
-author = 'Graziella'
+author = 'Justin Womack'
 
 release = '0.1'
 version = '0.1.0'
